a9daab52d488b3500e3a133827261986fb72f72f
Nick Celaya <nickcelaya07@gmail.com>
2020-08-27 00:18:04 -0600

    Refactored app component to class

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-const App = () => {
-	window.navigator.geolocation.getCurrentPosition(
-		(position) => console.log(position),
-		(err) => console.log(err)
-	);
-
-	return (
-		<div className='App'>
-			<div>Latitude:</div>
-		</div>
-	);
-};
+class App extends Component {
+	render() {
+		window.navigator.geolocation.getCurrentPosition(
+			(position) => console.log(position),
+			(err) => console.log(err)
+		);
+		return <div>Latitude: </div>;
+	}
+}
 
 export default App;
