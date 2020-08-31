f7cd5e45bb087abff8eefede919b0bfd80a82403
Nick Celaya <nickcelaya07@gmail.com>
2020-08-31 14:41:07 -0600

    Added error handling message to our app component state

--- src/App.js
+++ src/App.js
@@ -5,16 +5,27 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { latitude: '' };
+		this.state = { latitude: null, errorMessage: '' };
 
 		window.navigator.geolocation.getCurrentPosition(
 			position => this.setState({ latitude: position.coords.latitude }),
-			err => console.log(err)
+			err => {
+				this.setState({
+					errorMessage: err.message
+				});
+			}
 		);
 	}
 
 	render() {
-		return <div>Latitude: {this.state.latitude}</div>;
+		if (this.state.errorMessage && !this.state.latitude) {
+			return <div>{this.state.errorMessage}</div>;
+		}
+		if (!this.state.errorMessage && this.state.latitude) {
+			return <div>{this.state.latitude}</div>;
+		}
+
+		return <div>Loading!</div>;
 	}
 }
 export default App;
